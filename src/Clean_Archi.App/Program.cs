f202ea24ef0e010fb199b6a302b86edf25522753
Greg <ririo2@hotmail.fr>
2020-03-12 16:37:13 +0100

    initialized structure of the project

---
+++ src/Clean_Archi.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clean_Archi.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
